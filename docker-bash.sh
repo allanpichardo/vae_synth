6dc701a90d05ec585a01f38cedbe27f0a68ac19c
Allan Pichardo <allan.pichardo@gmail.com>
2021-09-12 02:37:08 -0400

    fixed user permissions

--- docker-bash.sh
+++ docker-bash.sh
@@ -1,1 +1,1 @@
-docker run --gpus all -it --rm -p 6006:6006 -v $PWD:/tmp -w /tmp mhz/tensorflow:latest bash
+docker run -u $(id -u):$(id -g) --runtime=nvidia -it --rm --gpus all --env-file .env -p 6006:6006 -v $PWD:/tmp -w /tmp mhz/tensorflow:latest bash
