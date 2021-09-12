a7de6b6953e1bbf7900cef2a855872ad72eb1ed8
Allan Pichardo <allan.pichardo@gmail.com>
2021-09-11 22:10:01 -0400

    updated script to use locally made mhz/tensorflow

--- docker-bash.sh
+++ docker-bash.sh
@@ -1,1 +1,1 @@
-docker run --gpus all -it --rm -p 6006:6006 -v $PWD:/tmp -w /tmp tensorflow/tensorflow:latest-gpu bash
+docker run --gpus all -it --rm -p 6006:6006 -v $PWD:/tmp -w /tmp mhz/tensorflow:latest bash
